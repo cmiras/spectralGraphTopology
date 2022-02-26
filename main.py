4f6c2aaefe14cf4ce718c950d267fa8594c07d71
bcollet <baptiste.collet@ens.psl.eu>
2022-02-26 16:55:11 +0100

    blockDiag

--- main.py
+++ main.py
@@ -33,5 +33,36 @@ def fscore(A,B):
                     fn+=1
             elif B[i][j]:
                 fp+=1
-    return (2*tp)/(2*tp+fn+fp)                           
+    return (2*tp)/(2*tp+fn+fp)
 print(L(np.array([2,3,2,2,2,2])))
+def pairwise_matrix_rownorm(M):
+    n=M.shape(0)
+    V=np.zero([n,n])
+    for i in range(n):
+        for j in range(i+1,n):
+            V[i][j]=np.linalg.norm(M[i]-M[j])**2
+    V+=V.T
+    return V
+def blockDiagCpp(matrices):
+    """matrcies:square np array list
+    return big np array diag by block with blocks given in matrices"""
+    n=matrices.shape[0]
+    sizes=[k.shape[0] for k in matrices]
+    N=sum(k)
+    if min(k.shape[0]==k.shape[1] for k in matrices):
+        raise raise ValueError('all matrices are not square.')
+    blockDiag=np.zeros([N,N])
+    i=0
+    for m in matrices:
+        n=m.shape[0]
+        for a in range(n):
+            for b in range(n):
+                blockDiag[i+a][i+b]=m[a][b]
+        i+=m.shape[0]
+    return blockDiag
+        
+def build_initial_graph(Y,m):
+    n=Y.shape[0]
+    A=np.zeros(n,n)
+def learn_k_component_graph(S,is_data_matrix=False, k=1,w0="naive",lb=0,ub=10**4,alpha=0,):
+    return None
